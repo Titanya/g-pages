83fdfb11529635a7573b86698a447221088f88e2
jouda_bouchahda <jbouchahda@anypli.com>
2017-03-12 18:29:34 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,7 +53,8 @@ export class AppComponent {
     if(!isSafari){
       console.log("non safari");
     //   window.open('tf1conso://'); app masbouba imchi w may7ilich store
-   let page =  window.open('tf1conso://');
+   //let page =  window.open('tf1conso://');
+   let page =  window.open('http://www.google.com');
   // let page =  window.open('https://www.w3schools.com/');
    //page.document.body.style.backgroundColor = "red";
   let bodyStyle = window.getComputedStyle(page.document.body, null);
