2beb94b4eeda19ef57c24b9c9bce528256e744e2
jouda_bouchahda <jbouchahda@anypli.com>
2017-03-13 14:24:28 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,8 +53,16 @@ export class AppComponent {
     if(!isSafari){
       console.log("non safari");
     //   window.open('tf1conso://'); app masbouba imchi w may7ilich store
- let page =  window.open('tf1conso://');
-  page.focus();
+ //let page =  window.open('tf1conso://');
+  //page.focus();
+  $.ajax({
+    url: "url",
+    type: "POST",
+    success: function() {
+       let page =  window.open('tf1conso://');             
+    }
+  });
+
   /// let page =  window.open('http://www.google.com');
   // let page =  window.open('https://www.w3schools.com/');
    //page.document.body.style.backgroundColor = "red";
